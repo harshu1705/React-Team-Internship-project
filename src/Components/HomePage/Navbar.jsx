da290db34582e36a836c70d7b8a216cfd0a12d93
Divya Gandhi <divyagandhi274@gmail.com>
2024-07-26 13:31:26 +0530

    Added assests

--- src/Components/HomePage/Navbar.jsx
+++ src/Components/HomePage/Navbar.jsx
@@ -23,9 +23,9 @@ function Navbar() {
 
   return (
     <div className='flex w-full'>
-      <div className='flex bg-white w-[30%] justify-between p-6'>
-        <p>Logo</p>
-        <p>Company</p>
+      <div className='flex w-[30%] justify-center gap-10 p-6'>
+        <p className='text-lg'> Logo</p>
+        <p className='text-lg '>Company Name</p>
       </div>
       <div
         className='flex bg-blue-700 w-[5%]'
@@ -39,9 +39,9 @@ function Navbar() {
       <div className='flex bg-blue-700 w-[60%] justify-between p-6 text-white '>
         <p className='hover:text-blue-300 cursor-pointer'>Home</p>
         <p className='hover:text-blue-300 cursor-pointer'>About Us</p>
-        <div className='flex gap-2 items-center relative'>
+        <div className='flex gap-2 relative'>
           <p className='hover:text-blue-300 cursor-pointer'>Menu</p>
-          <i className="fa-solid fa-angle-down mt-1 cursor-pointer" onClick={toggleMenu}></i>
+          <i className="fa-solid fa-angle-down  mt-1 cursor-pointer" onClick={toggleMenu}></i>
           {menuVisible && (
             <div className='absolute top-8 left-0 bg-white text-black p-2 shadow-lg w-24'>
               <p className='p-1 cursor-pointer hover:bg-blue-700 hover:text-white ' >Events</p>
