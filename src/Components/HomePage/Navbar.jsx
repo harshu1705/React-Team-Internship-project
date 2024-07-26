8cfc46705eb20059a8453d377b042a2c43beee1f
Divya Gandhi <divyagandhi274@gmail.com>
2024-07-26 11:36:53 +0530

    Navbar

--- src/Components/HomePage/Navbar.jsx
+++ src/Components/HomePage/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className='flex w-full'>
+      <div className='flex bg-white w-[30%] justify-between p-6'>
+        <p>Logo</p>
+        <p>Company</p>
+      </div>
+      <div
+        className='flex bg-blue-700 w-[5%] '
+        style={{ clipPath: 'polygon(100% 0, 100% 50%, 100% 100%, 5% 99%, 0 86%)' }}
+      >
+        
+      </div>
+      <div className='flex bg-blue-700 w-[5%]'>
+
+      </div>
+      <div className='flex bg-blue-700 w-[60%] justify-between p-6 text-white'>
+        <p>Home</p>
+        <p>About Us</p>
+        <p>Menu</p>
+        <p>Contact Us</p>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.clip-triangle': {
+          'clip-path': 'polygon(100% 0, 0 100%, 100% 100%)',
+        },
+        '.clip-polygon': {
+          'clip-path': 'polygon(50% 0%, 0% 100%, 100% 100%)',
+        },
+        '.clip-circle': {
+          'clip-path': 'circle(50% at 50% 50%)',
+        },
+        // Add more clip-path utilities if needed
+      };
 
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
+};
