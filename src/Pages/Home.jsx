06ace51e81c7407799b1b9b5f325044653241e64
Divya Gandhi <divyagandhi274@gmail.com>
2024-07-26 10:38:59 +0530

    Folder Set Up

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../Components/HomePage/Navbar'
+function Home() {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Home
