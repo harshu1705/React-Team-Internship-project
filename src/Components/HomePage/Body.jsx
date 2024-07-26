e968e113e405cd8c9a3ce1f61632a5658d819345
Divya Gandhi <divyagandhi274@gmail.com>
2024-07-26 16:02:04 +0530

    Added typing effect

--- src/Components/HomePage/Body.jsx
+++ src/Components/HomePage/Body.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+import Laptop from '../HomePage/Laptop (1).png';
 
 function Body() {
   return (
-    <div>
-        
+    <div className='flex w-full p-6'>
+      <div className='flex w-[50%] pl-20 flex-col pt-3'>
+        <ReactTypingEffect
+          text={['Website', 'Website Development', 'Website Solutions']}
+          speed={200}
+          eraseSpeed={200}
+          eraseDelay={200}
+          typingDelay={500}
+          className='text-[50px] font-bold'
+        />
+        <p className='mt-4'>
+          Welcome to TechSolve Solutions! We offer top-notch IT services including consulting, managed IT, and cloud solutions tailored to your business needs.
+        </p>
+      </div>
+      <div className='flex w-[50%] justify-center items-center'>
+        <img src={Laptop} alt='Laptop' className='max-w-full'/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Body
+export default Body;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Body from '../Components/HomePage/Body'
 import Navbar from '../Components/HomePage/Navbar'
 function Home() {
   return (
-    <div>
+    <div className='flex flex-col gap-2'>
         <Navbar/>
+        <Body/>
     </div>
   )
 }
