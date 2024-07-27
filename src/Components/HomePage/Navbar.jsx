a986fd52d86cf7061748e1c1b2a0ca90977476a1
Divya Gandhi <divyagandhi274@gmail.com>
2024-07-27 10:19:44 +0530

    Completed Project page,added scrollView in Contact Us and About Us

--- src/Components/HomePage/Navbar.jsx
+++ src/Components/HomePage/Navbar.jsx
@@ -11,8 +11,15 @@ function Navbar() {
   };
 
   const navigate = useNavigate();
+  function Event(){
+    navigate('/Event');
+  }
   function Project() {
     navigate('/Project');
+  }
+
+  function Home(){
+    navigate('/');
   }
 
   function Blog(){
@@ -20,6 +27,19 @@ function Navbar() {
   }
   function Review(){
     navigate('/Review');
+  }
+
+  const scrollToAboutUs = () => {
+    const aboutUsSection = document.getElementById('about-us-section');
+    if (aboutUsSection) {
+      aboutUsSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+  const scrollToContactUs=()=>{
+    const contactUsSection=document.getElementById('contact-us-section');
+    if(contactUsSection){
+        contactUsSection.scrollIntoView({behaviour:'smooth'});
+    }
   }
 
   return (
@@ -40,21 +60,21 @@ function Navbar() {
        
       </div>
       <div className='flex bg-blue-700 w-[60%] justify-between p-6 text-white '>
-        <p className='hover:text-blue-300 cursor-pointer font-bold'>Home</p>
-        <p className='hover:text-blue-300 cursor-pointer font-bold'>About Us</p>
+        <p className='hover:text-blue-300 cursor-pointer font-bold' onClick={Home}>Home</p>
+        <p className='hover:text-blue-300 cursor-pointer font-bold' onClick={scrollToAboutUs}>About Us</p>
         <div className='flex gap-2 relative'>
           <p className='hover:text-blue-300 cursor-pointer font-bold'>Menu</p>
           <i className="fa-solid fa-angle-down  mt-1 cursor-pointer" onClick={toggleMenu}></i>
           {menuVisible && (
-            <div className='absolute top-8 left-0 bg-white text-black p-2 shadow-lg w-24'>
-              <p className='p-1 cursor-pointer hover:bg-blue-700 hover:text-white ' >Events</p>
+            <div className='absolute top-8 left-0 bg-white text-black p-2 shadow-lg w-24 z-10'>
+              <p className='p-1 cursor-pointer hover:bg-blue-700 hover:text-white 'onClick={Event} >Events</p>
               <p className='p-1 cursor-pointer  hover:bg-blue-700 hover:text-white 'onClick={Project}>Projects</p>
               <p className='p-1 cursor-pointer  hover:bg-blue-700 hover:text-white ' onClick={Blog}>Blogs</p>
               <p className='p-1 cursor-pointer  hover:bg-blue-700 hover:text-white ' onClick={Review}>Reviews</p>
             </div>
           )}
         </div>
-        <p className='hover:text-blue-300 cursor-pointer font-bold'>Contact Us</p>
+        <p className='hover:text-blue-300 cursor-pointer font-bold' onClick={scrollToContactUs}>Contact Us</p>
       </div>
     </div>
   );
--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -6,13 +6,51 @@ import '../CSS/Carousel.css';
 
 function Project() {
   const images = [WebDev, PowerBI, WebDev, PowerBI, WebDev];
+  const ProjectCards=[
+    {
+        id:1,
+        title:"Frontend Project",
+        description:"This is a frontend development project with variety of styles,fonts,animations ",
+        image:WebDev,
+    },
+    {
+        id:2,
+        title:"Power BI Project",
+        description:"This is a Power BI project. It involves data visualization and creating interactive reports",
+        image:PowerBI,
+    },
+    {
+        id:3,
+        title:"Frontend Project",
+        description:"This is a frontend development project. It involves building the user interface and user experience",
+        image:WebDev,
+    },
+    {
+        id:4,
+        title:"Power BI Project",
+        description:"This is a Power BI project. It involves data visualization and creating interactive reports",
+        image:PowerBI,
+    },
+    {
+        id:5,
+        title:"Frontend Project",
+        description:"This is a frontend development project. It involves building the user interface and user experience",
+        image:WebDev,
+    },
+    {
+        id:4,
+        title:"Power BI Project",
+        description:"This is a Power BI project. It involves data visualization and creating interactive reports",
+        image:PowerBI,
+    },
+  ]
 
   return (
     <div className='flex flex-col '>
       <div>
         <Navbar />
       </div>
-      <p className='flex justify-center text-blue-700 text-[30px] font-bold mt-5'>Projects</p>
+      <p className='flex justify-center text-blue-700 text-[30px] font-bold mt-5 pb-3'>Projects</p>
       <div className='relative flex items-center justify-center bg-blue-700 p-3 overflow-hidden'>
         <div className='carousel'>
           {images.concat(images).map((image, index) => (
@@ -20,6 +58,26 @@ function Project() {
           ))}
         </div>
       </div>
+      <div className='flex justify-center mt-5 text-blue-700 text-[25px] font-bold'>MORE ABOUT PROJECTS</div>
+    <div className='flex  '>
+        
+        <div className='  grid grid-cols-3  w-full gap-5 p-5'>
+        {ProjectCards.map((Project) => (
+           <div className='flex  bg-blue-700 justify-between rounded-lg p-3'>
+             <div className='flex flex-col gap-3 w-[80%]'>
+             <p className='text-white text-bold text-[25px]'>{Project.title}</p>
+             <p className='text-white'>{Project.description}</p>
+            </div>
+            <div className='flex w-16'>
+                <img src={Project.image}/>
+             </div>   
+        </div>
+              
+            
+            ))}
+        </div>
+   
+    </div>
     </div>
   );
 }
