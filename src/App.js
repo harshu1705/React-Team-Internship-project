a6db37a79cc1c56daa614c3d1a66c75c406b19ee
Divya Gandhi <divyagandhi274@gmail.com>
2024-07-29 18:38:08 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from '../src/Pages/Home'
-import Blog from '../src/Pages/Blog'
-import Project from '../src/Pages/Project'
-import Event from '../src/Pages/Event'
-<<<<<<< HEAD
-import EventDetail from './Pages/EventDetail'
-import Review from './Pages/Review';
-=======
-import Research from '../src/Pages/Research'
+import Home from '../src/Pages/Home';
+import Blog from '../src/Pages/Blog';
+import Project from '../src/Pages/Project';
+import Event from '../src/Pages/Event';
 import EventDetail from './Pages/EventDetail';
->>>>>>> 2db0a788f64e9456ece780d2c505175205a06bc6
+import Research from '../src/Pages/Research'; 
+import Review from './Pages/Review';  
+
 function App() {
   return (
-   <Router>
+    <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/Blog" element={<Blog/>}/>
         <Route path="/Project" element={<Project/>}/>
         <Route path="/Event" element={<Event/>}/>
         <Route path="/event/:id" element={<EventDetail />} />
-<<<<<<< HEAD
-        <Route path="/Review" element={<Review />} />
-
-
-=======
-        <Route path="/Research" element={<Research />} />
->>>>>>> 2db0a788f64e9456ece780d2c505175205a06bc6
+        <Route path="/Research" element={<Research />} />  
+        <Route path="/Review" element={<Review />} />  
       </Routes>
-   </Router>
+    </Router>
   );
 }
 
